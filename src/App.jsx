41f5c8cf7a1cdf9b0839449bf42ecc8ad23f15c4
roesnera <adam.roesner@generation.org>
2023-05-04 11:23:31 -0400

    initial project commit, adds basic elements and some styling to hero

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+import Contact from "./Contact";
+import Hero from "./Hero";
+import Projects from "./Projects";
+
+function App() {
+
+  return (
+    <>
+      <Hero />
+      <Projects />
+      <Contact />
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Hero.css';
+
+export default function Hero() {
+  return <div className='hero'>
+    <div className="title-box">
+      <h1 className="title-text">Adam Roesner</h1>
+      <h2 className="subtitle">Full Stack Developer</h2>
+    </div>
+    <div className="svgs">
+
+    </div>
+    <p className="blurb">My name is Adam Roesner, I am a full stack developer based in New York City. Lorem ipsum dolor, sit amet consectetur adipisicing elit. At, sed? Excepturi laborum quibusdam magni debitis eaque nisi officiis corrupti quisquam voluptas eos commodi reprehenderit dolor ratione nesciunt sapiente, obcaecati porro!</p>
+    <div className="dot dot-1"></div>
+    <div className="dot dot-2"></div>
+
+  </div>;
+}
