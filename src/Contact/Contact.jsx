891ddeee5fa7b5144cf63343df213269131f6bdb
roesnera <adam.roesner@generation.org>
2023-05-05 16:20:12 -0400

    adds kind of working email form functionality- going to rework with nodemailer/mailgun

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -6,24 +6,41 @@ export default function Contact() {
   const [formEmail, setFormEmail] = useState("");
   const [formMessage, setFormMessage] = useState("");
 
+  const nameChange = (e) => {
+    e.preventDefault();
+    const newName = e.target.value;
+    setFormName(newName);
+  }
+
+  const emailChange = (e) => {
+    e.preventDefault();
+    const newEmail = e.target.value;
+    setFormEmail(newEmail);
+  }
+
+  const messageChange = (e) => {
+    e.preventDefault();
+    const newMessage = e.target.value;
+    setFormMessage(newMessage);
+  }
 
 
 
   return <section className='contact-section'>
     <h2 className='contact-header'>Send me a message</h2>
     <h4 className="contact-subheader">Have a question or proposal or just want to connect? Contact me here.</h4>
-    <form className='contact-form'>
+    <form className='contact-form' action='https://formsubmit.co/aaroesner@gmail.com' method='POST'>
       <div className="form-name">
         <label htmlFor="name">Your Name</label>
-        <input type="text" name="name" value={formName}/>
+        <input type="text" name="name" value={formName} onChange={nameChange}/>
       </div>
       <div className="form-email">
         <label htmlFor="email">Email Address</label>
-        <input type="text" name="email" value={formEmail}/>
+        <input type="text" name="email" value={formEmail} onChange={emailChange}/>
       </div>
       <div className="form-message">
-        <label htmlFor="message">Your Message</label>
-        <input type="text" name="message" value={formMessage}/>
+        <label htmlFor="text">Your Message</label>
+        <input type="text" name="text" value={formMessage} onChange={messageChange}/>
       </div>
       <button type='submit' className='form-button'>Send it</button>
     </form>
