5614e52373bd611ca1e23414f46c15cb78ef70e9
roesnera <adam.roesner@generation.org>
2023-05-16 21:29:57 -0400

    adding dynamic rendering for project cards

--- src/ProjectTile/ProjectTile.jsx
+++ src/ProjectTile/ProjectTile.jsx
@@ -1,30 +1,65 @@
 import React from 'react';
 import './ProjectTile.css';
 
-export default function ProjectTile({title}) {
+export default function ProjectTile({ title }) {
 
 
-  function getDeets() {
-    switch (title) {
-    case 'mars':
-      return (<div className='project-tile'>{title}</div>);
-    case 'Tech Fiends Forever':
-      return (<div className='project-tile'>{title}</div>);
-    case 'Some third thing':
-      return (<div className='project-tile'>{title}</div>);
-    default:
-      return null;
-    }
+  // function getDeets() {
+  //   switch (title) {
+  //   case 'mars':
+  //     return (<div className='project-tile'>{title}</div>);
+  //   case 'Tech Fiends Forever':
+  //     return (<div className='project-tile'>{title}</div>);
+  //   case 'Some third thing':
+  //     return (<div className='project-tile'>{title}</div>);
+  //   default:
+  //     return null;
+  //   }
+  // }
+
+  const details = title==="mars" ? {
+    title,
+    desription: "A promotional site for a social media influencer built based on a design spec.",
+    tools: [
+      "react",
+      "vite",
+      "html5",
+      "css3",
+      "js",
+    ]
+  }: title==="Tech Fiends Forever" ? {
+    title,
+    desription: "A blog site built with some of my colleagues front-to-back",
+    tools: [
+      "react",
+      "next13",
+      "html5",
+      "css3",
+      "js",
+      "supabase",
+    ]
+  }: {
+    title,
+    desription: "A placeholder project until I have a third one to share",
+    tools: [
+      "react",
+      "next13",
+      "html5",
+      "css3",
+      "js",
+      "supabase",
+      "docker"
+    ]
   }
-
 
   return (
     <div className='project-tile'>
       <div className='project-tile--img'>
-        {title}
+        {details.title}
       </div>
       <div className='project-tile--tech'>
-        React, HTML5, CSS3, JS
+        <div className="project-tile--tech__description">{details.desription}</div>
+        <div className="project-tile--tech__icons">{details.tools}</div>
       </div>
     </div>
   )
