5da93cd1fc7c59ac6baf23ca2cc364dff1e3f1a0
roesnera <adam.roesner@generation.org>
2023-06-12 16:32:23 -0400

    fixes resume link to link to an external site (def not needed but the fastest fix I could think of)

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <h3 className='footer-item'>Say Hello</h3>
     <div className="footer-item footer-list">
       <div className="footer-list-item">aaroesner@gmail.com</div>
-      <a className="footer-list-item" href="../../resume.html" target='_blank'>My Resume</a>
+      <a className="footer-list-item" rel="noreferrer" href="https://adamsapi.xyz/resume.html" target='_blank'>My Resume</a>
       <a className="footer-list-item" rel="noreferrer" href="https://www.github.com/roesnera" target="_blank">My GitHub</a>
     </div>
 
