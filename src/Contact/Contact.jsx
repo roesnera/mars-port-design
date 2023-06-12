b8dfcb973c0cd5cf5beb7a3a2dcd25b699760650
roesnera <adam.roesner@generation.org>
2023-06-12 16:03:03 -0400

    removes unused form fields

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -37,14 +37,14 @@ export default function Contact() {
     <h2 className='contact-header'>Send me a message</h2>
     <h4 className="contact-subheader">Have a question or proposal or just want to connect? Contact me here.</h4>
     <div className='contact-form'>
-      <div className="form-item form-name">
+      {/* <div className="form-item form-name">
         <label htmlFor="name">Your Name</label>
         <input type="text" name="name" value={formName} onChange={nameChange} placeholder='Enter your name'/>
       </div>
       <div className="form-item form-email">
         <label htmlFor="email">Email Address</label>
         <input type="text" name="email" value={formEmail} onChange={emailChange} placeholder='Enter your email address'/>
-      </div>
+      </div> */}
       <div className="form-item form-message">
         <label htmlFor="body">Your Message</label>
         <input type="text" name="text" value={formMessage} onChange={messageChange} placeholder='Hi, I think we need to work on the front end of our website . . .'/>
