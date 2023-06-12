e37db005d2e06b19f198a2efdb6df7faa4a50483
roesnera <adam.roesner@generation.org>
2023-06-12 16:14:39 -0400

    adds resume

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <h3 className='footer-item'>Say Hello</h3>
     <div className="footer-item footer-list">
       <div className="footer-list-item">aaroesner@gmail.com</div>
-      <div className="footer-list-item">My Resume</div>
+      <a className="footer-list-item" href="../../resume.html" target='_blank'>My Resume</a>
       <a className="footer-list-item" rel="noreferrer" href="https://www.github.com/roesnera" target="_blank">My GitHub</a>
     </div>
 
